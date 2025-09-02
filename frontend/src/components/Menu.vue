ce3d0810f6124f864af9495eb10fc12d501e1ebc
Lucas Tao <Lucashasanidea@gmail.com>
2025-09-02 13:07:17 +0930

    temp

---
+++ frontend/src/components/Menu.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="menu-bar">
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/about">About</a></li>
+      <li><a href="/services">Services</a></li>
+      <li><a href="/contact">Contact</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "MenuBar"
+}
+</script>
+
+<style scoped>
+.menu-bar {
+  background-color: #333;
+  padding: 0.5rem 1rem;
+}
+.menu-bar ul {
+  list-style: none;
+  display: flex;
+  gap: 1.5rem;
+  margin: 0;
+  padding: 0;
+}
+.menu-bar a {
+  color: white;
+  text-decoration: none;
+  font-weight: 500;
+}
+.menu-bar a:hover {
+  text-decoration: underline;
+}
+</style>
