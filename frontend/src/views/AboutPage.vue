64d1f1628f6a5434c1a14b05545c7d85c62d7693
Lucas Tao <Lucashasanidea@gmail.com>
2025-09-12 03:58:37 +0930

    quick fix, about page title

--- frontend/src/views/AboutPage.vue
+++ frontend/src/views/AboutPage.vue
@@ -2,3 +2,10 @@
     <h1>About Us</h1>
     <p>We are a company dedicated to providing the best services to our customers.</p>
 </template>
+
+<script setup>
+import { onMounted } from 'vue'
+onMounted(() => {
+  document.title = 'BetterInflux - About Us'
+})
+</script>
